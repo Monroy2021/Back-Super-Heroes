f8aa5a39f15abe5568099922b5e2c3fbc245274e
juan9507 <juandavidnaranjo75@gmail.com>
2022-07-24 09:06:48 -0500

    actualizacion creacion del modelo jugador

--- domain/model/src/main/java/co/com/gamehero/model/jugador/Jugador.java
+++ domain/model/src/main/java/co/com/gamehero/model/jugador/Jugador.java
@@ -1,11 +1,20 @@
 package co.com.gamehero.model.jugador;
+import co.com.gamehero.model.mazo.Mazo;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Jugador {
 
+    private String id;
     private String usuarioId;
+    private Mazo mazo;
+    private String alias;
+    private String puntaje;
 
 }
--- domain/model/src/main/java/co/com/gamehero/model/jugador/gateways/JugadorRepository.java
+++ domain/model/src/main/java/co/com/gamehero/model/jugador/gateways/JugadorRepository.java
@@ -1,4 +1,10 @@
 package co.com.gamehero.model.jugador.gateways;
 
+import co.com.gamehero.model.jugador.Jugador;
+import reactor.core.publisher.Mono;
+
 public interface JugadorRepository {
+
+    Mono<Jugador> save(Jugador jugador);
+
 }
